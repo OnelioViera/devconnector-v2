6658cb6a7bd8bfc77cfd8b41aff9f46eea401f51
Onelio Viera <ojvwebdesign@gmail.com>
2023-09-06 19:26:53 -0400

    update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,11 @@
+// Import the express.js library and the express.Router() function.
 const express = require('express');
 const router = express.Router();
 
+// Define the route for the GET request to api/auth.
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (rep, res) => res.send('Auth route'))
+router.get('/', (rep, res) => res.send('Auth route')) // This is the callback function that will be called when a GET request is made to the /api/auth  endpoint.
 
-module.exports = router;
+module.exports = router; // Export the router so it can be used in other parts of the application.
