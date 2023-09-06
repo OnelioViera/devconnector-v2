e11d5f61d760661bf4816bebabcb8f65fe426cdd
Onelio Viera <ojvwebdesign@gmail.com>
2023-09-06 19:37:19 -0400

    updated Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,26 +1,34 @@
+// Import Mongoose library for MongoDB schema modeling
 const mongoose =  require('mongoose')
 
+// Define the schema for the user collection
 const UserSchema = new mongoose.Schema({
+  // Name the user, a required string field
     name: {
         type: String,
         required: true
     },
+    // Email of the user, a required string field
     email:{
         type: String,
         required: true,
         unique: true
     },
+    // Password of the user, a required string field
     password:{
         type: String,
         required: true
     },
+    // Avatar of the user, a string field
     avatar:{
         type: String
     },
+    // Date the user was created, a date field with the value set to the current date
     date:{
         type: Date,
         default: Date.now
     }
 });
 
+// Export the user schema as a model
 model.exports = User = mongoose.model('user', UserSchema);
