2d368eb3810def657969176c23e9932498bd966c
Onelio Viera <ojvwebdesign@gmail.com>
2023-09-06 19:33:32 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+// Import the express.js library
 const express = require('express');
+// Import function to connect to the database
 const connectDB = require('./config/db');
 
+// Create an Express application instance
 const app = express();
 
-// Connect Database
+// Connect Database using the import function
 connectDB();
 
-// Init Middleware
+// Init Middleware to parse the JSON data from request body
 app.use(express.json({ extended: false }));
 
+// Define the default route that responds with 'API Running' when a GET request is made to the root URL: http://localhost:5000/
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
@@ -18,6 +22,8 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Define the port to run the server on
 const PORT = process.env.PORT || 5000;
 
+// Start the server andd listen on the specified port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
